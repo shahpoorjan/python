60cbecceeabe36ea1b75a4f01b8f6bf840597cfa
ahmad shah <shapoor_waheedy@hotmail.com>
2025-06-12 17:02:40 +0000

    while loop and for loop home exersise

---
+++ session3/hometask/for_loop.py
@@ -0,0 +1,35 @@
+# while True:
+#     ans= int(input("when was the computer invented? "))
+#     if ans < 2000:
+#         print("not correct")
+#     elif ans > 2000:
+#         print("it was too recent")
+#     elif ans == 2000:
+#         print ("correct")
+        
+# counter = 1
+# while counter < 11:
+#   print(counter)
+#   counter +=1
+# inp1= input("enter a word ? ")
+# inp2= input('enter a letter? ')
+# counter = 0
+# for char in inp1:
+#     if char == inp2:
+#         counter += 1
+# print (chari
+# inp=int(input("enter ur number"))
+# counter=20
+# while counter > inp:
+#     print(counter)
+#     counter-= 1
+# for i in range(2):
+#     for j in range(1,5):
+#         if j == 3:
+#             break
+#         else:
+#             print(j)
+#     print("--------")
+lst = ["hello" ,"world" ,"hello" , " test"]
+for i in lst:
+    print(f'{lst}')
